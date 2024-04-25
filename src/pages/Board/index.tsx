5003a5f1ef5d88d75bf2a419b551f4cef5e66d68
Yahuan Li <liyahuan@aragoncs.com>
2024-04-25 11:17:03 +0800

    feat: 获取动态路由的params参数和location,并在浏览器打印输出。

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,8 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 function Board (){
   const params = useParams();
-  const name = params.name
+  // useParams 可以拿到动态路由的值
+  //userLocation 可以获取到Url的信息
+  const name = params.name;
+  const location = useLocation();
+  console.log(params);
+  console.log(location)
+  const pram = `[${JSON.stringify(params)}]`
+  const loction = `[${JSON.stringify(location)}]`
+  console.log(`params: ${pram},location:${loction}`)
   return(
     <h1>
       Board,欢迎你 {name}!
