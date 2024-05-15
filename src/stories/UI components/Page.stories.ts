d88bc61a7bfbf972b11a675aa561e487124db095
Yahuan Li <liyahuan@aragoncs.com>
2024-05-15 18:01:11 +0800

    feat: add button component for storybooks

--- src/stories/UI components/Page.stories.ts
+++ src/stories/UI components/Page.stories.ts
@@ -1,10 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, expect } from '@storybook/test';
 
-import { Page } from './Page';
+import { Page } from '../Page';
 
 const meta = {
-  title: 'Example/Page',
+  title: 'Design System/Atoms/Page',
+  //title decided the folder directories
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
